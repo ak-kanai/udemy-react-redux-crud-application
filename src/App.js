ebcb1bd1b16110137f7242c721c78e6bb44d0b8e
ak-kanai <aki.kanai.1215@gmail.com>
2021-03-12 11:56:07 +0900

    learn jsx syntax

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
+import { Fragment } from 'react';
+
 function App() {
-  return <div><h1>Hello,world!</h1></div>;
+/*	
+  const greeting = "Hi!Tom";	
+  const dom = <h1 className="foo">{greeting}</h1>;	
+  return dom;
+*/
+ // return <input type="text" onClick={() => {console.log("I am clicked.")}} />;
+ // return <input type="text" onChange={() => {console.log("I am clicked.")}} />;
+  return (
+    <Fragment>
+      <label htmlFor="bar">bar</label>
+      <input type="text" onChange={() => {console.log("I am clicked.")}} />
+    </Fragment>	  
+  )
 }
 
 export default App;
