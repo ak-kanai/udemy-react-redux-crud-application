9b2dd331709329567958cdcf1b3df7fb34bc74c4
ak-kanai <aki.kanai.1215@gmail.com>
2021-03-16 09:59:50 +0900

    create class component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import { Fragment } from 'react';
-
+/*
 function App() {
-/*	
-  const greeting = "Hi!Tom";	
-  const dom = <h1 className="foo">{greeting}</h1>;	
-  return dom;
-*/
+	
+//  const greeting = "Hi!Tom";	
+//  const dom = <h1 className="foo">{greeting}</h1>;	
+//  return dom;
+
  // return <input type="text" onClick={() => {console.log("I am clicked.")}} />;
  // return <input type="text" onChange={() => {console.log("I am clicked.")}} />;
   return (
@@ -14,6 +14,15 @@ function App() {
       <input type="text" onChange={() => {console.log("I am clicked.")}} />
     </Fragment>	  
   )
+}
+*/
+
+const App = () => {
+  return <div><Cat /></div>
+}
+
+const Cat = () => {
+  return <div>Meow!</div>
 }
 
 export default App;
